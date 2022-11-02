76d808f90c8fb1720fe46d1f6261f0ed519949a8
Doha <hi.dohalee@gmail.com>
2022-11-02 19:31:06 +0900

    7 Nested Routes part One

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,19 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Route, Switch, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
-
-const Container = styled.div`
-	padding: 0px 20px;
-	max-width: 480px;
-	margin: 0 auto;
-`;
-
-const Header = styled.header`
-	height: 10vh;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-`;
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Title = styled.h1`
 	font-size: 48px;
@@ -23,6 +12,43 @@ const Title = styled.h1`
 const Loader = styled.span`
 	text-align: center;
 	display: block;
+`;
+
+const Container = styled.div`
+	padding: 0px 20px;
+	max-width: 480px;
+	margin: 0 auto;
+`;
+
+const Header = styled.header`
+	height: 15vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Overview = styled.div`
+	display: flex;
+	justify-content: space-between;
+	background-color: rgba(0, 0, 0, 0.5);
+	padding: 10px 20px;
+	border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	span:first-child {
+		font-size: 10px;
+		font-weight: 400;
+		text-transform: uppercase;
+		margin-top: 5px;
+	}
+`;
+
+const Description = styled.p`
+	margin: 20px 0px;
 `;
 
 interface RouteParams {
@@ -103,15 +129,54 @@ function Coin() {
 			).json();
 			setInfo(infoData);
 			setPriceInfo(priceData);
+			setLoading(false);
 		})();
 	}, []);
 
 	return (
 		<Container>
 			<Header>
-				<Title>{state.name || "Loading..."}</Title>
+				<Title>{state?.name || "Loading..."}</Title>
 			</Header>
-			{loading ? <Loader>Loading...</Loader> : null}
+			{loading ? (
+				<Loader>Loading...</Loader>
+			) : (
+				<>
+					<Overview>
+						<OverviewItem>
+							<span>Rank:</span>
+							<span>{info?.rank}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Symbol:</span>
+							<span>${info?.symbol}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Open Source:</span>
+							<span>{info?.open_source}</span>
+						</OverviewItem>
+					</Overview>
+					<Description>{info?.description}</Description>
+					<Overview>
+						<OverviewItem>
+							<span>Total Suply:</span>
+							<span>{priceInfo?.total_supply}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Max Suply:</span>
+							<span>{priceInfo?.max_supply}</span>
+						</OverviewItem>
+					</Overview>
+					<Switch>
+						<Route path={`/${coinId}/price`}>
+							<Price />
+						</Route>
+						<Route path={`/${coinId}/chart`}>
+							<Chart />
+						</Route>
+					</Switch>
+				</>
+			)}
 		</Container>
 	);
 }
